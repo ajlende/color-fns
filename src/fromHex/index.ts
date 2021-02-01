b138e854063f9f24168820c978d0ac7c7256995e
Alex Lende <ajlende@gmail.com>
2021-01-31 20:58:18 -1000

    Trim the input string in fromHex

--- src/fromHex/index.ts
+++ src/fromHex/index.ts
@@ -1,38 +1,42 @@
 import { Hex, Color } from "../types"
 
 /**
- * Convert a hex string to an RGB object, discarding any alpha component.
+ * Convert a hex string to a Color
  *
- * @param color Hex string
+ * @see https://www.w3.org/TR/css-color-4/#hex-notation
  *
- * @return Color in the intermediary format or null if the color could not be parsed
+ * @param c Hex string
+ *
+ * @return Color in the intermediary format
  *
  * @throws {TypeError}
- * Thrown if the input string is not a valid hex format.
+ * Thrown if the input string is not a valid hex format
  */
 export default function fromHex(color: Hex): Color {
+	const c = color.trim()
+
 	let r = 0x00
 	let g = 0x00
 	let b = 0x00
 	let a = 0xff
 
 	/* eslint-disable no-fallthrough */
-	switch (color.length) {
+	switch (c.length) {
 		// Parse #RRGGBBAA and #RRGGBB strings.
 		case 9:
-			a = Number(`0x${color[7]}${color[8]}`)
+			a = Number(`0x${c[7]}${c[8]}`)
 		case 7:
-			r = Number(`0x${color[1]}${color[2]}`)
-			g = Number(`0x${color[3]}${color[4]}`)
-			b = Number(`0x${color[5]}${color[6]}`)
+			r = Number(`0x${c[1]}${c[2]}`)
+			g = Number(`0x${c[3]}${c[4]}`)
+			b = Number(`0x${c[5]}${c[6]}`)
 			break
 		// Parse #RGBA and #RGB strings.
 		case 5:
-			a = Number(`0x${color[4]}${color[4]}`)
+			a = Number(`0x${c[4]}${c[4]}`)
 		case 4:
-			r = Number(`0x${color[1]}${color[1]}`)
-			g = Number(`0x${color[2]}${color[2]}`)
-			b = Number(`0x${color[3]}${color[3]}`)
+			r = Number(`0x${c[1]}${c[1]}`)
+			g = Number(`0x${c[2]}${c[2]}`)
+			b = Number(`0x${c[3]}${c[3]}`)
 			break
 		default:
 			throw new RangeError("Color is not a valid hex format.")
