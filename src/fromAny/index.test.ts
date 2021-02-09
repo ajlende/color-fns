44e2e23c56e4dc70b08cd7c5f09fc19b1354be91
Alex Lende <ajlende@gmail.com>
2021-02-08 15:05:11 -1000

    Add types to fromAny test

--- src/fromAny/index.test.ts
+++ src/fromAny/index.test.ts
@@ -1,12 +1,13 @@
 import test from "ava"
 
+import { Vec4, Hex, RgbString } from "../types"
 import fromAny from "."
 
 Object.entries({
 	// Add every way of representing white here
-	Hex: "#fff",
-	Vec4: [1, 1, 1, 1],
-	RgbString: "rgba(255, 255, 255, 1)",
+	Hex: "#fff" as Hex,
+	Vec4: [1, 1, 1, 1] as Vec4,
+	RgbString: "rgba(255, 255, 255, 1)" as RgbString,
 }).forEach(([type, value]) => {
 	test(`parses ${type} type`, (t) => {
 		t.deepEqual(fromAny(value), [1, 1, 1, 1])
