eb7d3687512ff9f5d94b6ef08f15d7111a70ff8c
Alex Lende <alex+github.com@lende.xyz>
2025-04-16 08:35:55 -0500

    Fix todo in docs

--- src/format/types.ts
+++ src/format/types.ts
@@ -1,9 +1,10 @@
 import type { Color } from "../types.js"
 
 /**
- * TODO: Think of a better name. This contains a function to test if a value is
- * of a given type and then convert that type to the intermediate format.
+ * This contains a function to test if a value is of a given type and then
+ * convert that type to the intermediate format.
  */
+// TODO: Think of a better name. 
 export interface ColorType<T> {
 	/**
 	 * Check to see if the type of the input is T so it can be converted later.
