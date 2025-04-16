d17e167fa26ba8dbbe2d85b94a9166f6f5a749cc
Alex Lende <alex+github.com@lende.xyz>
2025-04-15 20:22:25 -0500

    Eslint ignore .gitignore

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,17 @@
 // @ts-check
 
+import { fileURLToPath, URL } from "node:url"
+
 import eslint from "@eslint/js"
-import tseslint from "typescript-eslint"
+import { includeIgnoreFile } from "@eslint/compat"
 import ava from "eslint-plugin-ava"
 import prettier from "eslint-config-prettier/flat"
+import tseslint from "typescript-eslint"
+
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url))
 
 export default tseslint.config(
+	includeIgnoreFile(gitignorePath),
 	eslint.configs.recommended,
 	tseslint.configs.recommended,
 	ava.configs["flat/recommended"],
